7f3b38e96b12102d7b3c8c2e959d2f51d8987916
sklearn21 <sklearn21@gmail.com>
2021-03-21 11:16:50 +0530

    url error.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 
@@ -8,3 +8,15 @@ from blog.models import Post
 def post_list_view(request):
     post_list = Post.objects.all()
     return render(request, 'blog/post_list.html', {'post_list': post_list}) 
+
+def post_detail_view(request,year,month,day,post):
+    post=get_object_or_404(
+        Post, 
+        slug=post, 
+        status='published', 
+        publish_year=year, 
+        publish_month=month, 
+        publish_day=day
+        )
+
+    return render(request, 'blog/post_detail.html', {'post':post})
