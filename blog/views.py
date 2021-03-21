6610eef8fe05192bfe9fed8197d24ccfcf0916e6
sklearn21 <sklearn21@gmail.com>
2021-03-21 17:37:39 +0530

    Pagination function based views.

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
 
 
 def post_list_view(request):
     post_list = Post.objects.all()
+    paginator = Paginator(post_list,1)
+    page_number = request.GET.get('page')
+    try:
+        post_list=paginator.page(page_number)
+    except PageNotAnInteger:
+        post_list=paginator.page(1)
+    except EmptyPage:
+        post_list=paginator.page(paginator.num_pages)
     return render(request, 'blog/post_list.html', {'post_list': post_list})
 
 
